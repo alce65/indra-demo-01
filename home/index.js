03075151436a83d9bd30b927e7d31a917cd850a1
Alejandro Cerezo <alce65@hotmail.es>
2025-09-25 11:43:46 +0200

    Update is in home page

--- home/index.js
+++ home/index.js
@@ -1,1 +1,6 @@
 console.log("Loaded index js")
+
+const button = document.createElement("button")
+button.innerText = "Click me"
+button.onclick = () => alert("Hello from index.js!")
+document.body.appendChild(button)
